9f5b4231e0a8518cc68853cb0750da9dfc7bede0
Edward <duongedward44@gmail.com>
2022-06-23 16:23:59 -0500

    Added current players, change current player, cannot click on occupied cell

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-
 let initialState;
 const gameState = {
-  players: ['x', 'o'],
+  players: ["x", "o"],
   board: [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
-  ]
-}
+    [null, null, null],
+  ],
+  currentPlayer: "x",
+};
 
 //Grabbing the board from HTML
 const board = document.getElementById("board");
 
-
-board.addEventListener('click', (event) => {
-
-  event.target.innerHTML = gameState.players[0]
-
+board.addEventListener("click", (event) => {
+ if (!event.target.innerHTML){event.target.innerHTML = gameState.currentPlayer};
   // alternate X and O's by Utilizing a If statement
+  if (gameState.currentPlayer === "x") {
+    //update currentplayer to be O
+    gameState.currentPlayer = "o"
+  } else {gameState.currentPlayer = "x"
+  
+  
+    }
+  
 
-  // makes sure if someone clicks on the same square 
+  // makes sure if someone clicks on the same square
   // as an occupied square it doesn't change.
   // makes sure that the boards match up.
-
-} );
-
+});
