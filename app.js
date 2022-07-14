f62eaed89c8b368387a0240283e9677f0c9ac030
Edward <duongedward44@gmail.com>
2022-07-14 17:31:27 -0500

    change checkWinner

--- app.js
+++ app.js
@@ -8,21 +8,19 @@ const gameState = {
   ],
   currentPlayer: "x",
 };
-
-
-
+const status = document.getElementById("status");
+const restart = document.getElementById("restart");
 const board = document.getElementById("board");
-let cells = document.getElementsByClassName("cell");
+// let cells = document.getElementsByClassName("cell");
 
 board.addEventListener("click", (event) => {
   if (!event.target.innerHTML) {
     event.target.innerHTML = gameState.currentPlayer;
   }
 
-let currentId = event.target.id
-let [rowId, colId] = currentId.split("")
-gameState.board[rowId][colId]=gameState.currentPlayer
-  
+  let currentId = event.target.id;
+  let [rowId, colId] = currentId.split("");
+  gameState.board[rowId][colId] = gameState.currentPlayer;
 
   if (gameState.currentPlayer === "x") {
     gameState.currentPlayer = "o";
@@ -31,7 +29,6 @@ gameState.board[rowId][colId]=gameState.currentPlayer
   }
   let result = checkWinner(gameState.board);
 });
-
 
 function getRow(board, row) {
   return board[row];
@@ -43,71 +40,55 @@ function getColumn(board, column) {
   return col;
 }
 
-function getDiag1(board, x, y) {
+function getDiag1(board) {
   let diag1 = [board[0][0], board[1][1], board[2][2]];
   return diag1;
 }
 
-function getDiag2(board, x, y) {
+function getDiag2(board) {
   let diag2 = [board[0][2], board[1][1], board[2][0]];
   return diag2;
 }
 
 function checkArrays(array) {
-  console.log(array)
   if (array.join("") === "xxx") {
-    return "x is winner";
+    console.log("x is winner");
   }
   if (array.join("") === "ooo") {
-    return "o is winner";
+    console.log("o is winner");
   }
-  return false;
 }
 
 function checkWinner(board) {
-  for (let i = 0; i < board.length; i++) {
-    let row = getRow(board, i);
-    let winner = checkArrays(row);
-    if (winner === "x is winner") {
-      return "x win";
-    }
-    if (winner === "o is winner") {
-      return "o win";
-    }
-  }
+  checkArrays(getColumn(board, 0));
+  checkArrays(getColumn(board, 1));
+  checkArrays(getColumn(board, 2));
+  checkArrays(getRow(board, 0));
+  checkArrays(getRow(board, 0));
+  checkArrays(getRow(board, 0));
+  checkArrays(getDiag1(board));
+  checkArrays(getDiag2(board));
 
-  for (let i = 0; i < board.length; i++) {
-    let column = getColumn(board, i);
-    let winner = checkArrays(column);
-    if (winner === "x is winner") {
-      return "x win";
-    }
-    if (winner === "o is winner") {
-      return "o win";
-    }
-  }
+  //console.log("XDDDE",firstDiag, secondDiag)
 
-  for (let i = 0; i < board.length; i++) {
-    let diag1 = getDiag1(board, i);
-    let winner = checkArrays(diag1);
-    if (winner === "x is winner") {
-      return "x win";
-    }
-    if (winner === "o is winner") {
-      return "o win";
-    }
-  }
-  for (let i = 0; i < board.length; i++) {
-    let diag2 = getDiag2(board, i);
-    let winner = checkArrays(diag2);
-    if (winner === "x is winner") {
-      return "x win";
-    }
-    if (winner === "o is winner") {
-      return "o win";
-    }
-  }
-  return false;
+  // for (let i = 0; i < board.length; i++) {
+  //   let diag1 = getDiag1(board, i);
+  //   let winner = checkArrays(diag1);
+  //   if (winner === "x is winner") {
+  //     return "x win";
+  //   }
+  //   if (winner === "o is winner") {
+  //     return "o win";
+  //   }
+  // }
+  // for (let i = 0; i < board.length; i++) {
+  //   let diag2 = getDiag2(board, i);
+  //   let winner = checkArrays(diag2);
+  //   if (winner === "x is winner") {
+  //     return "x win";
+  //   }
+  //   if (winner === "o is winner") {
+  //     return "o win";
+  //   }
+  // }
 }
-
-
